4d94eda762559379d80a0df11ef35cb697b8c380
Slathia <slathiasachin13@gmail.com>
2018-08-07 13:11:56 +0530

    Java Assignment 1

--- 07-08-2018/assignment-java/src/main/java/com/knoldus/CountWord.java
+++ 07-08-2018/assignment-java/src/main/java/com/knoldus/CountWord.java
@@ -1,6 +1,8 @@
 package com.knoldus;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -9,12 +11,17 @@ public class CountWord {
 
     public static void main(String[] args) {
 
-        String input = "sdaasd dasd as  Dasd sa";
+        String input = "sdaazxdas a a a a as  Dasd sa";
+        List<List<Object>> list = new ArrayList<>();
 
-
-        Map<String, Integer> wordCounter1 = Stream.of(input).map(w -> w.split("\\s+"))
+        //Counts every element in list
+        Map<String, Integer> wordCounter = Stream.of(input).map(w -> w.split("\\s+"))
                 .flatMap(Arrays::stream)
                 .collect(Collectors.toMap(w -> w.toLowerCase(), w -> 1, Integer::sum));
-        System.out.println(wordCounter1);
+
+        //Adds map tuples to list
+        wordCounter.keySet().forEach(ele -> list.add(Arrays.asList(ele, wordCounter.get(ele))));
+
+        System.out.println(list);
     }
 }
--- 07-08-2018/assignment-java/src/main/java/com/knoldus/MultiplyList.java
+++ 07-08-2018/assignment-java/src/main/java/com/knoldus/MultiplyList.java
@@ -1,27 +1,21 @@
-/*
 package com.knoldus;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class MultiplyList {
 
+
     public static void main(String[] args) {
 
-        List list1 = Arrays.asList(1, 2, 3, 4, 5);
-        List list2 = Arrays.asList(1, 2, 32, 421, 5, 9);
+        List<Integer> firstList = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> secondList = Arrays.asList(1, 2, 32, 421, 5, 9);
 
-        System.out.println(list1);
-        System.out.println(list2.size());
+        List<Integer> resultList = new ArrayList<>();
+        firstList.forEach(num -> secondList.forEach(num2 -> resultList.
+                add(Math.multiplyExact(num, num2))));
 
-        if (list1.size() == list2.size()) {
-            List list3 = list1.stream().map(s1 -> list2.
-                    stream().map(s3 -> (Integer) s3 * (Integer) s1)).collect(Collectors.toList());
-
-        } else {
-            System.out.println("Two list can only be multiplied when there size is same");
-        }
+        System.out.println(resultList);
     }
 }
-*/
